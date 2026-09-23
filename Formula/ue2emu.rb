9bab76402627d14ceafc7cd1481e5bba122e503d
Alex Damhuis <alex.damhuis@icloud.com>
2026-09-23 09:38:29 +0200

    ue2emu 0.4.5

--- Formula/ue2emu.rb
+++ Formula/ue2emu.rb
@@ -3,8 +3,8 @@
 class Ue2emu < Formula
   desc "Emulator for Ultimate 64 Elite II and C64 Ultimate firmware"
   homepage "https://github.com/Jondalar/UE2-C64U-Emulator"
-  url "https://github.com/Jondalar/UE2-C64U-Emulator/archive/refs/tags/v0.4.4.tar.gz"
-  sha256 "9e652be96408eb5f7ac27730ecc029a6e8e53cf1bfe562f11f6e95e492a1082c"
+  url "https://github.com/Jondalar/UE2-C64U-Emulator/archive/refs/tags/v0.4.5.tar.gz"
+  sha256 "2d3f5b62253827c1723e819634b06f3a2704e1e819da967b8b596ca5cd1cfb77"
   license "GPL-3.0-or-later"
   head "https://github.com/Jondalar/UE2-C64U-Emulator.git", branch: "main"
 
